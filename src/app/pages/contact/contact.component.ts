46e873c1e2dd3f0f8c53c7c5d5fc58fec122fba7
kellyncodes <kellyncodes@gmail.com>
2023-12-17 02:12:57 +0100

    inst: ngrx toastr for notification alert. feat: contact page, both design and logic done.

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { PageHeaderComponent } from '../../components/page-header/page-header.component';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { HttpStatusCode } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+import { MatError } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'sosinc-contact',
@@ -15,4 +26,67 @@ import { PageHeaderComponent } from '../../components/page-header/page-header.co
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  public contactForm!: FormGroup;
+  public isSending = signal<boolean>(false);
+
+  constructor(private fb: FormBuilder, private toastr: ToastrService) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.pattern('^\\+(?:[0-9] ?){6,14}[0-9]$')]],
+      date: [new Date().toDateString(), Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  isInputValid(controlName: string, errorName: string): boolean {
+    if (
+      this.contactForm.controls[controlName]?.touched &&
+      this.contactForm.controls[controlName]?.hasError(errorName)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  async onSubmit(): Promise<void> {
+    if (!this.contactForm.valid) {
+      this.toastr.error('Please fill all the fields');
+      return;
+    }
+    try {
+      this.isSending.set(true);
+      const response = await emailjs.send(
+        'service_angzgfi',
+        'template_em27ivs',
+        {
+          to_name: 'there',
+          ...this.contactForm.value,
+        },
+        'qivBRNuQUCrknS0_k'
+      );
+      if (response.status != HttpStatusCode.Ok) {
+        this.isSending.set(false);
+        this.toastr.error(
+          'Something unexpected happened while sending the message.Please try again.'
+        );
+        return;
+      }
+      if (response.status == HttpStatusCode.Ok) {
+        this.isSending.set(false);
+        this.toastr.success('We have received your message.');
+        this.contactForm.reset();
+        return;
+      }
+    } catch (error: any) {
+      if (error?.status != HttpStatusCode.Ok) {
+        this.isSending.set(false);
+        this.toastr.error('Message not sent. Try again.');
+        return;
+      }
+    }
+  }
+}
